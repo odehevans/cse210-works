eaaa4aa0a54e145ce46b7809e3b4d9ed114d43b0
Mapopo (Evans) <mppdgreat@gmail.com>
2023-02-28 21:09:13 +0100

    prepare: Learning 02 - abstraction

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Developer";
+        job1._company = "Microsoft";
+        job1._startYear = 2003;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Fullstack Developer";
+        job2._company = "Apple";
+        job2._startYear = 2010;
+        job2._endYear = 2020;
+
+        Resume resume = new Resume();
+        resume._name = "Falz Maco";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
