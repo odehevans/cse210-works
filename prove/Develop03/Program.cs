5a14e9fd09994afcd9a4fb4d7e222ff2e11dcdbc
Mapopo (Evans) <mppdgreat@gmail.com>
2023-03-14 00:07:39 +0100

    Exceed requirements

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -2,24 +2,54 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {   string text = "Trust in the Lord With all thine heart and lean not unto thine understanding; in all things aknowledge Him and He will direct thy path.";
-        Reference reference = new Reference("Proverbs", 3, 5, 6);
-        Scripture scripture = new Scripture(reference, text);
+  static List<Scripture> _scriptures = new List<Scripture>();
+  static Random _random = new Random();
+  static void Main(string[] args)
+  {
+    // Showing Creativity and Exceeding Requirements
+    string[] lines = System.IO.File.ReadAllLines("scriptures.txt");
+    foreach (string line in lines)
+    {
+      string[] parts = line.Split("::");
+      string text = parts[0];
+      string book = parts[1];
+      int chapter = int.Parse(parts[2]);
+      int verse = int.Parse(parts[3]);
 
-        while(!scripture.AllHidden()) {
-            Console.Clear();
+      Reference reference;
 
-            scripture.Display();
 
-            Console.WriteLine();
-            Console.WriteLine("Press enter to randomly hide words or quit to exit ");
-            string reply = Console.ReadLine();
 
-            if (!String.IsNullOrEmpty(reply) && reply.ToLower() == "quit") break;
+      if (parts.Length > 4)
+      {
+        int endVerse = int.Parse(parts[3]);
+        reference = new Reference(book, chapter, verse, endVerse);
+      }
+      else
+      {
+        reference = new Reference(book, chapter, verse);
+      }
 
-            scripture.hideRandomWord();
-            scripture.hideRandomWord();
-        }
+      _scriptures.Add(new Scripture(reference, text));
     }
+
+    int index = _random.Next(_scriptures.Count);
+    Scripture scripture = _scriptures[index];
+
+    while (!scripture.AllHidden())
+    {
+      Console.Clear();
+
+      scripture.Display();
+
+      Console.WriteLine();
+      Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+      string reply = Console.ReadLine();
+
+      if (!String.IsNullOrEmpty(reply) && reply.ToLower() == "quit") break;
+
+      scripture.hideRandomWord();
+      scripture.hideRandomWord();
+    }
+  }
 }
